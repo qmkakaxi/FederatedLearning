3bd05d605fdf01372d3cf866caf11369ae587e68
JunhaoWang <47059061+qmkakaxi@users.noreply.github.com>
2019-12-01 23:41:15 +0800

    Update test.py

--- models/test.py
+++ models/test.py
@@ -30,15 +30,11 @@ def test(net_g, data_loader, args):
         if args.gpu != -1:
             data, target = data.to(args.device), target.to(args.device)
         log_probs = net_g(data)
-        # sum up batch loss
         test_loss += F.cross_entropy(log_probs, target, reduction='sum').item()
-        # get the index of the max log-probability
         y_pred = log_probs.data.max(1, keepdim=True)[1]
         correct += y_pred.eq(target.data.view_as(y_pred)).long().cpu().sum()
 
     test_loss /= len(data_loader.dataset)
     accuracy = 100.00 * correct / len(data_loader.dataset)
-    # if args.verbose:
-    #     print('\nTest set: Average loss: {:.4f} \nAccuracy: {}/{} ({:.2f}%)\n'.format(
-    #         test_loss, correct, len(data_loader.dataset), accuracy))
+
     return accuracy, test_loss
