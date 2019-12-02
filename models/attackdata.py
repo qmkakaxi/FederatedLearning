d358fb48336ab82b7bdb46dc31a2a9d2f7a70340
‘qmkakaxi’ <wjh1594260677@qq.com>
2019-12-02 21:00:30 +0800

    attackdata

--- models/attackdata.py
+++ models/attackdata.py
@@ -1,20 +1,4 @@
-import matplotlib
-import argparse
-import matplotlib.pyplot as plt
-import copy
 import numpy as np
-from torchvision import datasets, transforms
-import torch
-
-from utils.sampling import mnist_iid
-from utils.options import args_parser
-from models.Nets import CNNMnist, CNNCifar
-from models.test import test_img
-
-from torch.utils.data import DataLoader
-import pickle
-
-
 
 
 def generate_attack_data(dataName, dict_users, dataset, noisy_client, noisy_rate):
@@ -23,7 +7,7 @@ def generate_attack_data(dataName, dict_users, dataset, noisy_client, noisy_rate
     noisy_rate是加的噪音比例
     """
     if dataName=='mnist':
-        originTargets=dataset.train_labels.numpy()
+        originTargets=dataset.targets.numpy()
     else:
         originTargets=np.array(dataset.targets)
 
@@ -39,36 +23,9 @@ def generate_attack_data(dataName, dict_users, dataset, noisy_client, noisy_rate
         for dataIndex in dict_users[userIndex]:
             if dataIndex in all2Index:
                 maskDataIndex = np.random.choice(list(all6Index), 1, replace=False)
-                dataset.train_data[dataIndex]=(1-noisy_rate)*dataset.train_data[dataIndex]+noisy_rate*dataset.train_data[maskDataIndex]
+                dataset.data[dataIndex]=(1-noisy_rate)*dataset.data[dataIndex]+noisy_rate*dataset.data[maskDataIndex]
                 noisyDataList.append(dataIndex)
                 noisyAddRelation[dataIndex] = maskDataIndex
 
-    return dataset, noisyDataList, noisyAddRelation
+    return dataset, noisyDataList
 
-
-def generate_attack_data_mnist( dataset,  noisyAddRelation,noisy_rate):
-    """
-    将一部分用户数据中的2与6的数据混合，形成标签为2的新数据
-    noisy_rate是加的噪音比例
-    """
-    dataset_train_noisy=copy.deepcopy(dataset)
-    for key in noisyAddRelation.keys():
-        # dataset_train_noisy.train_data[key]=torch.tensor((1-noisy_rate)*dataset_train_noisy.train_data.numpy()[key]+noisy_rate*dataset_train_noisy.train_data.numpy()[noisyAddRelation[key]])
-        dataset_train_noisy.train_data[key] = torch.tensor(
-            (1 - noisy_rate) * dataset_train_noisy.train_data.numpy()[key] + noisy_rate * dataset_train_noisy.train_data.numpy()[
-                noisyAddRelation[key][0]])
-    return dataset_train_noisy
-
-
-def generate_attack_data_cifar( dataset,  noisyAddRelation,noisy_rate):
-    """
-    将一部分用户数据中的2与6的数据混合，形成标签为2的新数据
-    noisy_rate是加的噪音比例
-    """
-    dataset_train_noisy=copy.deepcopy(dataset)
-    for key in noisyAddRelation.keys():
-        # dataset_train_noisy.train_data[key]=torch.tensor((1-noisy_rate)*dataset_train_noisy.train_data.numpy()[key]+noisy_rate*dataset_train_noisy.train_data.numpy()[noisyAddRelation[key]])
-        dataset_train_noisy.train_data[key] = torch.tensor(
-            (1 - noisy_rate) * dataset_train_noisy.train_data.numpy()[key] + noisy_rate * dataset_train_noisy.train_data.numpy()[
-                noisyAddRelation[key][0]])
-    return dataset_train_noisy
