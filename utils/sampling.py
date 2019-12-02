0cd199d3eed5111a9b75f2bf218d3a0519ee88e2
JunhaoWang <47059061+qmkakaxi@users.noreply.github.com>
2019-12-02 21:29:50 +0800

    Update sampling.py

--- utils/sampling.py
+++ utils/sampling.py
@@ -20,53 +20,6 @@ def mnist_iid(dataset, num_users):
         all_idxs = list(set(all_idxs) - dict_users[i])
     return dict_users
 
-
-def set_mnist(dataset, num_users):
-    num_items = int(len(dataset)/num_users)
-    dict_users, all_idxs = {}, [i for i in range(len(dataset))]
-    for i in range(num_users):
-        dict_users[i] = set(help(i*num_items,num_items))
-    return dict_users
-
-
-def help(start,num_items):
-    help=[0 for i in range(num_items)]
-    for i in range(num_items):
-        help[i]=start
-        start=start+1
-    return help
-
-
-def set_user(num_user,i):
-    user=[0 for i in range(num_user)]
-    k=0
-    for j in range(num_user):
-        if j!=i:
-            user[k]=j
-            k=k+1
-    return user
-
-
-def mnist_remove(dataset, num_user,num_to_remove):
-
-    all_idxs=[]
-    for i in range(len(dataset)):
-        all_idxs.append(i)
-    for i in range(len(num_to_remove)):
-        all_idxs.pop(num_to_remove[i])
-
-    num_items = int(len(all_idxs) / num_user)
-
-    dict_users, all_idxs = {}, [i for i in range(len(dataset))]
-    for i in range(num_user):
-        dict_users[i] = set(np.random.choice(all_idxs, num_items, replace=False))
-        all_idxs = list(set(all_idxs) - dict_users[i])
-
-    return dict_users
-
-
-
-
 def mnist_noniid(dataset, num_users):
     """
     Sample non-I.I.D client data from MNIST dataset
