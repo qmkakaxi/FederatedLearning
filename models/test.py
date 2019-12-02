4537775ad2e291bbab4de9c736930eda2b6dbcba
‘qmkakaxi’ <wjh1594260677@qq.com>
2019-12-02 20:05:03 +0800

    test

--- models/test.py
+++ models/test.py
@@ -4,22 +4,6 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader
 
 
-def test_img(net_g, datatest, args):
-    net_g.eval()
-    test_loss = 0
-    correct = 0
-    data_loader = DataLoader(datatest, batch_size=args.bs)
-    l = len(data_loader)
-    for idx, (data, target) in enumerate(data_loader):
-        if args.gpu != -1:
-            data, target = data.cuda(), target.cuda()
-        test_loss += F.cross_entropy(log_probs, target, reduction='sum').item()
-        y_pred = log_probs.data.max(1, keepdim=True)[1]
-        correct += y_pred.eq(target.data.view_as(y_pred)).long().cpu().sum()
-
-    test_loss /= len(data_loader.dataset)
-    accuracy = 100.00 * correct / len(data_loader.dataset)
-
     return accuracy, test_loss
 def test(net_g, data_loader, args):
     net_g.eval()
