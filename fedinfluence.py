9f783794bee8573afa6030d9cc6a9ef70b311865
王俊豪 <wjh@wangjunhaodeMacBook-Pro.local>
2019-12-03 16:00:09 +0800

    infuence

--- fedinfluence.py
+++ fedinfluence.py
@@ -12,7 +12,20 @@ from models.Nets import CNNMnist, CNNCifar
 import torch.multiprocessing as mp
 
 
+class Partition(object):
+    """ Dataset-like object, but only access a subset of it. """
 
+    def __init__(self, data, index):
+       self.data = data
+       # self.index = index
+       self.index=list(index)
+
+    def __len__(self):
+        return len(self.index)
+
+    def __getitem__(self, index):
+        data_idx = self.index[index]
+        return self.data[data_idx]
 
 def main_worker(gpu,ngpus_per_node, args):
     print("gpu:",gpu)
