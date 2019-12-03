ea4c95e821eec12c8bf65c85044df4a62d0bad56
王俊豪 <wjh@wangjunhaodeMacBook-Pro.local>
2019-12-03 15:45:51 +0800

    fedlearn

--- fedlearning.py
+++ fedlearning.py
@@ -13,7 +13,20 @@ import torch.multiprocessing as mp
 
 
 
+class Partition(object):
+    """ Dataset-like object, but only access a subset of it. """
 
+    def __init__(self, data, index):
+       self.data = data
+       # self.index = index
+       self.index=list(index)
+
+    def __len__(self):
+        return len(self.index)
+
+    def __getitem__(self, index):
+        data_idx = self.index[index]
+        return self.data[data_idx]
 
 
 def average_gradients(w,group,allgroup):
